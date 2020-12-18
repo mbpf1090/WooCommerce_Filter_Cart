83acef906cac7334fe4de14ba9d7fa366bdd602d
mbpf1090 <sebastian.weidele@gmail.com>
2020-12-18 11:56:03 +0000

    add save options and cart filter update

--- card-filter-admin.php
+++ card-filter-admin.php
@@ -1,7 +1,7 @@
 <?php settings_errors(); ?>
 
 <form method="post" action="options.php" class="sunset-general-form">
-	<?php settings_fields( 'category-settings-options' ); ?>
+	<?php settings_fields( 'category-settings' ); ?>
 	<?php do_settings_sections( 'filter-cart' ); ?>
 	<?php submit_button(); ?>
 </form>
--- cart_filter.php
+++ cart_filter.php
@@ -9,6 +9,7 @@
 if (!defined('ABSPATH')) {
 	exit; // Exit if accessed directly
 }
+
 // Filter
 add_filter( 'woocommerce_cart_item_visible', 'mbpf_hide_category_from_cart', 10, 3 );
 
@@ -17,15 +18,16 @@ function mbpf_hide_category_from_cart($visible, $cart_item, $cart_item_key) {
 	$product = apply_filters( 'woocommerce_cart_item_product', $cart_item['data'], $cart_item, $cart_item_key);
 	$category_set = get_term_by( 'id', $product->category_ids[0], 'product_cat', 'ARRAY_A' );
 	$category = $category_set['name'];
-
-	if ( strcmp("Zutat", $category ) == 0 ) {
-		$visible = false;
+	$categories = get_option('category_option');
+	foreach ($categories as $cat => $cat_value) {
+		if ( strcmp($cat, $category ) == 0 ) {
+			$visible = false;
+		}
 	}
-	$cats = get_all_categories();
-	echo var_dump($cats);
 	return $visible;
 }
 
+// Helper functions
 function get_product_category_name_by_id( $category_id ) {
 	$term = get_term_by( 'id', $category_id, 'product_cat', 'ARRAY_A' );
 	return $term['name'];
@@ -49,24 +51,27 @@ add_action('admin_init', 'cart_filter_settings');
 add_action('admin_menu', 'cart_filter_admin_menu');
 
 function cart_filter_settings() {
-	register_setting('category-settings', 'category_option');
-	add_settings_section('category-settings-options', 'Cart Options', 'category_settings_callback', 'filter-cart');
-	add_settings_field('category_option', 'Categories', 'category_selection_callback', 'filter-cart', 'category-settings-options');
+//	register_setting( string $option_group, string $option_name, array $args = array()  )
+	register_setting( 'category-settings', 'category_option' );
+//	add_settings_section( string $id, string $title, callable $callback, string $page  )
+	add_settings_section( 'category-settings-options', 'Cart Options', 'category_settings_callback', 'filter-cart' );
+//	add_settings_field( string $id, string $title, callable $callback, string $page, string $section = 'default', array $args = array() )<Paste>
+	add_settings_field( 'category-option', 'Categories', 'category_selection_callback', 'filter-cart', 'category-settings-options' );
 }
 
 function category_settings_callback() {
-	echo 'hallo';
+	echo 'Check the categories to hide from cart';
 }
 
 function category_selection_callback() {
 	$categories = get_all_categories();
 	$selected_categories = get_option('category_option');
-	//var_dump($selected_categories);
 	$output = '';
-	$checked = false;
-	foreach ($categories as $format) {
-		$format = $format->name;
-		$output .= '<label><input type="checkbox" id="'.$format.'" name="post_formats['.$format.']" value="1" '.$checked.' /> '.$format.'</label><br>';
+	foreach ($categories as $category) {
+		$checked = (@$selected_categories[$category->name] == 1 ? 'checked' : '');
+		$category = $category->name;
+
+		$output .= '<label><input type="checkbox" id="'.$category.'" name="category_option['.$category.']" value="1" '.$checked.' /> '.$category.'</label><br>';
 	}
 	echo $output;	
 }
