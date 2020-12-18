07ed0566797dda43aea4add77668eeddb3de6e0c
mbpf1090 <sebastian.weidele@gmail.com>
2020-12-18 08:32:28 +0000

    add basic admin menu

---
+++ card-filter-admin.php
@@ -0,0 +1,7 @@
+<?php settings_errors(); ?>
+
+<form method="post" action="options.php" class="sunset-general-form">
+	<?php settings_fields( 'category-settings-options' ); ?>
+	<?php do_settings_sections( 'filter-cart' ); ?>
+	<?php submit_button(); ?>
+</form>
--- cart_filter.php
+++ cart_filter.php
@@ -9,7 +9,7 @@
 if (!defined('ABSPATH')) {
 	exit; // Exit if accessed directly
 }
-
+// Filter
 add_filter( 'woocommerce_cart_item_visible', 'mbpf_hide_category_from_cart', 10, 3 );
 
 
@@ -21,7 +21,8 @@ function mbpf_hide_category_from_cart($visible, $cart_item, $cart_item_key) {
 	if ( strcmp("Zutat", $category ) == 0 ) {
 		$visible = false;
 	}
-
+	$cats = get_all_categories();
+	echo var_dump($cats);
 	return $visible;
 }
 
@@ -29,3 +30,55 @@ function get_product_category_name_by_id( $category_id ) {
 	$term = get_term_by( 'id', $category_id, 'product_cat', 'ARRAY_A' );
 	return $term['name'];
 }
+
+function get_all_categories() {
+	$orderby = 'name';
+	$order = 'asc';
+	$hide_empty = false ;
+	$cat_args = array(
+    		'orderby'    => $orderby,
+    		'order'      => $order,
+    		'hide_empty' => $hide_empty,
+	);
+
+	return get_terms( 'product_cat', $cat_args );
+}
+
+// Menu
+add_action('admin_init', 'cart_filter_settings');
+add_action('admin_menu', 'cart_filter_admin_menu');
+
+function cart_filter_settings() {
+	register_setting('category-settings', 'category_option');
+	add_settings_section('category-settings-options', 'Cart Options', 'category_settings_callback', 'filter-cart');
+	add_settings_field('category_option', 'Categories', 'category_selection_callback', 'filter-cart', 'category-settings-options');
+}
+
+function category_settings_callback() {
+	echo 'hallo';
+}
+
+function category_selection_callback() {
+	$categories = get_all_categories();
+	$selected_categories = get_option('category_option');
+	//var_dump($selected_categories);
+	$output = '';
+	$checked = false;
+	foreach ($categories as $format) {
+		$format = $format->name;
+		$output .= '<label><input type="checkbox" id="'.$format.'" name="post_formats['.$format.']" value="1" '.$checked.' /> '.$format.'</label><br>';
+	}
+	echo $output;	
+}
+
+
+
+function cart_filter_admin_menu() {
+	add_menu_page('Cart Filter', 'Filter Options', 'manage_options', 'filter-cart', 'create_menu_page', 'dashicons-cart', 4 );
+}
+
+function create_menu_page() {
+	require_once( dirname(__FILE__) . '/card-filter-admin.php' );
+}
+
+
